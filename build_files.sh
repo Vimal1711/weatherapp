5d1bb39c1c87a268be9df2a18494ee89670a8990
Vimal1711 <kuwarvimal1711@gmail.com>
2023-12-20 14:50:43 +0530

    firstchange

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
-python3.9 -m pip install -r requirements.yxt
+python3.9 -m pip install -r requirements.txt  # Corrected filename to 'requirements.txt'
 python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
